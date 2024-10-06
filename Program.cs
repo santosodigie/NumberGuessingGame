9cd9b514a133933f4807cc4e66e07f4ad82ffb7f
Santos Odigie <santosodigie104@gmail.com>
2024-10-06 23:37:45 +0100

    Add basic number guessing game logic with user input and random number generation.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to my number guessing game");
+
+Random rnd = new Random();
+
+int randomnum = rnd.Next(1, 20);
+
+bool exit = false;
+
+while (!exit)
+{
+    Console.WriteLine("Please guess a number between 1 and 20! ");
+    int guess = int.Parse(Console.ReadLine());
+
+    if (guess == randomnum)
+    {
+        Console.WriteLine("Congrats you guessed the number!");
+        exit = true;
+    }
+    else if (guess < randomnum)
+    {
+        Console.WriteLine("Guess higher");
+    }
+    else if (guess > randomnum)
+    {
+        Console.WriteLine("Guess lower");
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid number");
+    }
+}
